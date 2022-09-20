b6adc02bbf7ac41a2581e54265b79309497da393
isorokina <93764917+isorokina@users.noreply.github.com>
2022-09-20 14:20:26 +0300

    lab2

--- cat.py
+++ cat.py
@@ -2,8 +2,38 @@ class Cat:
     name=None
     age=None
     isHappy=None
-cat1=Cat()
-cat1.name="Milka"
+
+    def __init__(self, name=None, age=None, isHappy=None):
+        self.name=name
+        self.age=age
+        self.isHappy=isHappy
+
+        self.get_data()
+
+    def set_data(self, name=None, age=None, isHappy=None):
+        self.name=name
+        self.age=age
+        self.isHappy=isHappy
+
+    def get_data(self):
+        print(self.name, 'age:', self.age, 'Happy:', self.isHappy)
+
+
+
+cat1=Cat('Toms')
+
+'''cat1=Cat('Toms', 3, True)
+cat2=Cat('Džeri', 7)
+
+cat1.set_data('Toms', 3, True)
+cat2=Cat()
+cat2.set_data('Džeri', 7, False)
+cat1.get_data()
+cat2.get_data()'''
+
+
+
+'''cat1.name="Milka"
 cat1.age=3
 cat1.isHappy=True
 print(cat1.name, cat1.age,cat1.isHappy )
@@ -12,4 +42,4 @@ cat2=Cat()
 cat2.name="Toms"
 cat2.age=5
 cat2.isHappy=False 
-print(cat2.name, cat2.age,cat2.isHappy )
+print(cat2.name, cat2.age,cat2.isHappy )'''
