137b85c3e272e94969fc028539d7daecc2566e5f
isorokina <93764917+isorokina@users.noreply.github.com>
2022-09-20 13:53:36 +0300

    lab2

---
+++ cat.py
@@ -0,0 +1,15 @@
+class Cat:
+    name=None
+    age=None
+    isHappy=None
+cat1=Cat()
+cat1.name="Milka"
+cat1.age=3
+cat1.isHappy=True
+print(cat1.name, cat1.age,cat1.isHappy )
+
+cat2=Cat()
+cat2.name="Toms"
+cat2.age=5
+cat2.isHappy=False 
+print(cat2.name, cat2.age,cat2.isHappy )
